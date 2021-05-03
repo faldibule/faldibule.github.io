f3f5f1a198face432199c7dd91432faec9fd3dc9
faldibule <faldibule@gmail.com>
2021-05-03 11:43:09 +0700

    Memisahkan script AOS dan memberikan beberapa perubahan pada gallery dan project

---
+++ js/myAos.js
@@ -0,0 +1,14 @@
+//gallery
+const imgGallery = document.querySelectorAll(".img-gallery");
+for (let i = 0; i < imgGallery.length; i++) {
+  imgGallery[i].setAttribute("data-aos", "fade-left");
+  imgGallery[i].setAttribute("data-aos-offset", "400");
+}
+
+//project
+const projectAnimation = document.querySelectorAll(".project-animation");
+for (let i = 0; i < projectAnimation.length; i++) {
+  projectAnimation[i].setAttribute("data-aos-offset", "400");
+}
+
+AOS.init({ once: true });
